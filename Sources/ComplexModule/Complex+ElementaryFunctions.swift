ec6793fb96df077c101bb33fec88c34f60b1f9ad
Hayden McCabe <hayden@haydenmccabe.com>
2020-07-05 20:49:45 -0700

    Refined atanh

--- Sources/ComplexModule/Complex+ElementaryFunctions.swift
+++ Sources/ComplexModule/Complex+ElementaryFunctions.swift
@@ -16,33 +16,33 @@ import RealModule
 #if !os(Windows)
 extension Complex: ElementaryFunctions {
     public static func exp(_ x: Self) -> Self {
-        let a = Complex(RealType.cos(x.imaginary), RealType.sin(x.imaginary))
-        return a.multiplied(by: RealType.exp(x.real))
+        let a = Complex(.cos(x.imaginary), .sin(x.imaginary))
+        return a.multiplied(by: .exp(x.real))
     }
 
     // Todo: replace
     public static func expMinusOne(_ x: Self) -> Self {
-        Complex.exp(x - 1)
+        .exp(x - .one)
     }
 
     @_transparent
     public static func pow(_ x: Self, _ y: Self) -> Self {
-        Complex.exp(y * Complex.log(x))
+        .exp(y * .log(x))
     }
 
     @_transparent
     public static func pow(_ x: Self, _ n: Int) -> Self {
-        Complex.exp(Complex.log(x).multiplied(by: RealType(n)))
+        .exp(Complex.log(x).multiplied(by: RealType(n)))
     }
 
     @_transparent
     public static func sqrt(_ x: Self) -> Self {
-        Complex(RealType.cos(x.phase/2), RealType.sin(x.phase/2)).multiplied(by: RealType.sqrt(x.length))
+        Complex(RealType.cos(x.phase/2), .sin(x.phase/2)).multiplied(by: .sqrt(x.length))
     }
 
     @_transparent
     public static func root(_ x: Self, _ n: Int) -> Self {
-        Complex.exp(Complex.log(x).divided(by: RealType(n)))
+        .exp(Complex.log(x).divided(by: RealType(n)))
     }
 
     @_transparent
@@ -50,14 +50,31 @@ extension Complex: ElementaryFunctions {
         Complex(RealType.log(x.length), x.phase)
     }
 
-    // Todo : replace
+    /// This function uses a Taylor series to find an approximation
+    /// of ln(1 + x) for very small x, i.e., for values close to zero.
     public static func log(onePlus x: Self) -> Self {
-        Complex.log(x + 1)
+        let maxIterations = 40
+        var n = 0
+        var approx = x
+        var prior = x
+        var z = x
+        let zz = x * x
+        var f = 1
+        repeat {
+            n += 1
+            prior = approx
+            z *= zz
+            f *= (2*n) * (2*n + 1)
+            let term = z.multiplied(by: 1 / RealType(f))
+            approx += (n % 2 == 0) ? term : -term
+        } while approx != prior && n < maxIterations
+        print(n)
+        return approx
     }
 
     @_transparent
     public static func cos(_ x: Self) -> Self {
-        Complex.cosh(Complex(-x.imaginary, x.real))
+        .cosh(Complex(-x.imaginary, x.real))
     }
 
     @_transparent
@@ -70,66 +87,121 @@ extension Complex: ElementaryFunctions {
     public static func tan(_ x: Self) -> Self {
         let tanh = Complex.tanh(Complex(-x.imaginary, x.real))
         let a = Complex(tanh.imaginary, -tanh.real)
-        return a == -Complex.i ? -a : a
+        return a == -.i ? -a : a
     }
 
     @_transparent
     public static func cosh(_ x: Self) -> Self {
         Complex( 
-            RealType.cosh(x.real) * RealType.cos(x.imaginary),
-            RealType.sinh(x.real) * RealType.sin(x.imaginary)
+            .cosh(x.real) * .cos(x.imaginary),
+            .sinh(x.real) * .sin(x.imaginary)
         )
     }
 
     @_transparent
     public static func sinh(_ x: Self) -> Self {
         Complex(
-            RealType.sinh(x.real) * RealType.cos(x.imaginary),
-            RealType.cosh(x.real) * RealType.sin(x.imaginary)
+            .sinh(x.real) * .cos(x.imaginary),
+            .cosh(x.real) * .sin(x.imaginary)
         )
     }
 
     @_transparent
     public static func tanh(_ x: Self) -> Self {
-        let a = Complex.sinh(x) / Complex.cosh(x)
-        return a.isZero ? Complex.one : a
+        let a = .sinh(x) / .cosh(x)
+        return a.isZero ? .one : a
     }
 
     @_transparent
     public static func acos(_ x: Self) -> Self {
-        let a = sqrt((x * x) - Complex.one)
-        let b = -Complex.i * Complex.log(x + a)
-        return (x.real.sign != x.imaginary.sign) ? b * -Complex.one : b
+        let a = sqrt((x * x) - .one)
+        let b = -.i * .log(x + a)
+        return (x.real.sign != x.imaginary.sign) ? b * -.one : b
     }
 
     @_transparent
     public static func asin(_ x: Self) -> Self {
-        let a = Complex.sqrt(Complex.one - (x * x))
-        return -Complex.i * Complex.log(Complex.i * x + a)
+        // For values close to 0, use
+        // a Taylor series approximation
+        if x.length <= RealType(1)/2 {
+            let maxIterations = 40
+            // Initialize variables
+            // to their state after adding the
+            // 0th term of the series
+            var approx = x
+            var n = 0
+            var k: RealType = 1
+            var z = x
+            let zz = x*x
+            var prior = approx
+            repeat {
+                prior = approx
+                n += 1
+                let a = 2 * n - 1
+                k *= RealType(a) / RealType(a+1)
+                z *= zz
+                approx += z.multiplied(by: k / RealType(a + 2))
+            } while prior != approx && n < maxIterations
+            return approx
+        } else {
+            let a = Complex(RealType.pi/2) - .acos(x)
+            return a.isZero ? x : a
+        }
     }
 
     @_transparent
     public static func atan(_ x: Self) -> Self {
-        let a = (Complex.i - x) / (Complex.i + x)
-        return -Complex.log(a) * Complex.i.divided(by: 2)
+        //if x.length < RealType(1)/2 {
+        if true {
+            let maxIterations = 40
+            var approx = x
+            var prior = x
+            var n = 0
+            var z = x
+            let zz = x * x
+            repeat {
+                n += 1
+                prior = approx
+                z *= zz
+                let term = z.divided(by: RealType(2 * n + 1))
+                approx += (n % 2 == 0) ? term : -term
+            } while prior != approx && n < maxIterations
+            return approx
+        } else {
+            let a = (.i - x) / (.i + x)
+            return -.log(a) * Complex.i.divided(by: 2)
+        }
     }
 
    @_transparent
     public static func acosh(_ x: Self) -> Self {
         let a = Complex.i.multiplied(by: 
             RealType(signOf: x.imaginary, magnitudeOf: 1))
-        return a * Complex.acos(x)
+        return a * .acos(x)
     }
     
     @_transparent
     public static func asinh(_ x: Self) -> Self {
-        -Complex.i * Complex.asin(Complex.i * x)
+        -.i * .asin(.i * x)
     }
 
     @_transparent
     public static func atanh(_ x: Self) -> Self {
-        -Complex.i * Complex.atan(Complex.i * x)
- 
+        return -.i * .atan(.i * x)
+        let maxIterations = 40
+        var approx = x
+        var prior = x
+        var n = 0
+        var z = x
+        let zz = x * x
+        repeat {
+            n += 1
+            prior = approx
+            z *= zz
+            let term = z.divided(by: RealType(2 * n + 1))
+            approx += term
+        } while prior != approx && n < maxIterations
+        return approx
     }
 }
 
--- Tests/ComplexTests/ComplexElementaryTests.swift
+++ Tests/ComplexTests/ComplexElementaryTests.swift
@@ -16,8 +16,8 @@ import RealModule
 #if !os(Windows)
 final class ComplexElementaryTests: XCTestCase {
 
-  func randomComplex<T>(_ type: T.Type) -> Complex<T> where T: BinaryFloatingPoint, T: Real,
-  T.Exponent: FixedWidthInteger, T.RawSignificand: FixedWidthInteger {
+    func randomComplex<T>(_ type: T.Type) -> Complex<T> where T: BinaryFloatingPoint, T: Real,
+    T.Exponent: FixedWidthInteger, T.RawSignificand: FixedWidthInteger {
     func randomReal() -> T {
       let exponentRange =
       (T.leastNormalMagnitude.exponent + T.Exponent(T.significandBitCount)) ...
@@ -27,53 +27,58 @@ final class ComplexElementaryTests: XCTestCase {
       return T.init(sign: .plus, exponent: randomExp, significand: randomSig)
     }
     return Complex<T>(randomReal(), randomReal())
-  }
-
-
-  // MARK: Log accuracy tests
-  func testLogAccuracy<T>(accuracy: T)
-  where T: BinaryFloatingPoint, T: ComplexFunctionReal,
-        T.Exponent: FixedWidthInteger, T.RawSignificand: FixedWidthInteger  {
-    for _ in 1...10 {
-      let argument = randomComplex(T.self)
-      let result = Complex.log(argument)
-      let correct = Complex.log_builtin(argument)
-
-      // If both values are non-finite, consider that a success
-      if !result.isFinite && !correct.isFinite {
-        return
-      }
-
-      // If the values are both finite, compare them
-      if result.isFinite, correct.isFinite,
-      relativeError(result, correct) > accuracy {
-        XCTFail()
-        print(Mirror(reflecting: T.self).description)
-        print("Argument: \(argument)\ncalculated: \(result)\nbuiltin: \(correct)")
-        let error = relativeError(result, correct)
-        print ("Relative error: \(error)")
-        print("\n")
-      }
     }
-  }
 
-  func testLogAccuracy() {
+
+    // MARK: Log accuracy tests
+    func testLogAccuracy<T>(accuracy: T)
+    where T: BinaryFloatingPoint, T: ComplexFunctionReal,
+        T.Exponent: FixedWidthInteger, T.RawSignificand: FixedWidthInteger  {
+        for _ in 1...10 {
+          let argument = randomComplex(T.self)
+          let result = Complex.log(argument)
+          let correct = Complex.log_builtin(argument)
+
+          // If both values are non-finite, consider that a success
+          if !result.isFinite && !correct.isFinite {
+            return
+          }
+
+          // If the values are both finite, compare them
+          if result.isFinite, correct.isFinite,
+          relativeError(result, correct) > accuracy {
+            XCTFail()
+            print(Mirror(reflecting: T.self).description)
+            print("Argument: \(argument)\ncalculated: \(result)\nbuiltin: \(correct)")
+            let error = relativeError(result, correct)
+            print ("Relative error: \(error)")
+            print("\n")
+          }
+        }
+    }
+
+    func testLogAccuracy() {
     testLogAccuracy(accuracy: Float(16))
     testLogAccuracy(accuracy: Double(16))
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
     testLogAccuracy(accuracy: Float80(16))
     #endif
+    }
 
-  }
+    func testLogPlusAccuracy() {
+        let arg = Complex<Double>(0.00000001)
+        let ans = Complex.log(onePlus: arg)
+        print(ans)
+    }
 
-  // MARK: Accuracy tests
-  // Compare the accuracy of ComplexModule's operations
-  // against those provided from CLang
-  func testFunctionAccuracy<T>(
-    _ a: (Complex<T>) -> (Complex<T>), 
+    // MARK: Accuracy tests
+    // Compare the accuracy of ComplexModule's operations
+    // against those provided from CLang
+    func testFunctionAccuracy<T>(
+    _ a: (Complex<T>) -> (Complex<T>),
     _ b: (Complex<T>) -> (Complex<T>),
     accuracy: T)
-  where T: BinaryFloatingPoint, T: ComplexFunctionReal,
+    where T: BinaryFloatingPoint, T: ComplexFunctionReal,
         T.Exponent: FixedWidthInteger, T.RawSignificand: FixedWidthInteger  {
     for _ in 1...100 {
       let argument = randomComplex(T.self)
@@ -89,132 +94,133 @@ final class ComplexElementaryTests: XCTestCase {
       if result.isFinite, correct.isFinite,
       relativeError(result, correct) > accuracy {
         print(Mirror(reflecting: T.self).description)
-        print("Argument: \(argument)\ncalculated: \(result)\nbuiltin:    \(correct)")
+        print("Argument:   \(argument)\ncalculated: \(result)\nbuiltin:    \(correct)")
         let error = relativeError(result, correct)
         print ("Relative error: \(error)")
         print("\n")
         XCTFail()
       }
     }
-  }  
+    }
 
 
-  func testExpAccuracy() {
+
+    func testExpAccuracy() {
     testFunctionAccuracy(Complex<Float>.exp, Complex<Float>.exp_builtin, accuracy: 16)
     testFunctionAccuracy(Complex<Double>.exp, Complex<Double>.exp_builtin, accuracy: 16)
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
     testFunctionAccuracy(Complex<Float80>.exp, Complex<Float80>.exp_builtin, accuracy: 16)
     #endif
-  }
+    }
 
-  func testSqrtAccuracy() {
+    func testSqrtAccuracy() {
     testFunctionAccuracy(Complex<Float>.sqrt, Complex<Float>.sqrt_builtin, accuracy: 16)
     testFunctionAccuracy(Complex<Double>.sqrt, Complex<Double>.sqrt_builtin, accuracy: 16)
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
     testFunctionAccuracy(Complex<Float80>.sqrt, Complex<Float80>.sqrt_builtin, accuracy: 16)
     #endif
-  }
-  
-  // TODO: Check Float80
-  func testCosAccuracy() {
+    }
+
+    // TODO: Check Float80
+    func testCosAccuracy() {
     testFunctionAccuracy(Complex<Float>.cos, Complex<Float>.cos_builtin, accuracy: 16)
     testFunctionAccuracy(Complex<Double>.cos, Complex<Double>.cos_builtin, accuracy: 16)
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
     //testFunctionAccuracy(Complex<Float80>.cos, Complex<Float80>.cos_builtin, accuracy: 16)
     #endif
-  }
-  
-  func testSinAccuracy() {
+    }
+
+    func testSinAccuracy() {
     testFunctionAccuracy(Complex<Float>.sin, Complex<Float>.sin_builtin, accuracy: 16)
     testFunctionAccuracy(Complex<Double>.sin, Complex<Double>.sin_builtin, accuracy: 16)
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
     testFunctionAccuracy(Complex<Float80>.sin, Complex<Float80>.sin_builtin, accuracy: 16)
     #endif
-  }  
-  
-  func testTanAccuracy() {
+    }
+
+    func testTanAccuracy() {
     testFunctionAccuracy(Complex<Float>.tan, Complex<Float>.tan_builtin, accuracy: 16)
     testFunctionAccuracy(Complex<Double>.tan, Complex<Double>.tan_builtin, accuracy: 16)
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
     testFunctionAccuracy(Complex<Float80>.tan, Complex<Float80>.tan_builtin, accuracy: 16)
     #endif
-  } 
-  
-  // Check Float80
-  func testCoshAccuracy() {
+    }
+
+    // Check Float80
+    func testCoshAccuracy() {
     testFunctionAccuracy(Complex<Float>.cosh, Complex<Float>.cosh_builtin, accuracy: 16)
     testFunctionAccuracy(Complex<Double>.cosh, Complex<Double>.cosh_builtin, accuracy: 16)
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
     //testFunctionAccuracy(Complex<Float80>.cosh, Complex<Float80>.cosh_builtin, accuracy: 16)
     #endif
-  }
-  
-  func testSinhAccuracy() {
+    }
+
+    func testSinhAccuracy() {
     testFunctionAccuracy(Complex<Float>.sinh, Complex<Float>.sinh_builtin, accuracy: 16)
     testFunctionAccuracy(Complex<Double>.sinh, Complex<Double>.sinh_builtin, accuracy: 16)
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
     testFunctionAccuracy(Complex<Float80>.sinh, Complex<Float80>.sinh_builtin, accuracy: 16)
     #endif
-  } 
-  
-  
-  func testTanhAccuracy() {
+    }
+
+
+    func testTanhAccuracy() {
     testFunctionAccuracy(Complex<Float>.tanh, Complex<Float>.tanh_builtin, accuracy: 16)
     testFunctionAccuracy(Complex<Double>.tanh, Complex<Double>.tanh_builtin, accuracy: 16)
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
     testFunctionAccuracy(Complex<Float80>.tanh, Complex<Float80>.tanh_builtin, accuracy: 16)
     #endif
-  }
-  
-  func testAcosAccuracy() {
+    }
+
+    func testAcosAccuracy() {
     testFunctionAccuracy(Complex<Float>.acos, Complex<Float>.acos_builtin, accuracy: 16)
     testFunctionAccuracy(Complex<Double>.acos, Complex<Double>.acos_builtin, accuracy: 16)
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
     testFunctionAccuracy(Complex<Float80>.acos, Complex<Float80>.acos_builtin, accuracy: 16)
     #endif
-  }
+    }
 
-  // TODO: Check calculation
-  func testASinAccuracy() {
+    // TODO: Check calculation
+    func testASinAccuracy() {
     testFunctionAccuracy(Complex<Float>.asin, Complex<Float>.asin_builtin, accuracy: 16)
     testFunctionAccuracy(Complex<Double>.asin, Complex<Double>.asin_builtin, accuracy: 16)
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
-    //testFunctionAccuracy(Complex<Float80>.asin, Complex<Float80>.asin_builtin, accuracy: 16)
+    testFunctionAccuracy(Complex<Float80>.asin, Complex<Float80>.asin_builtin, accuracy: 16)
     #endif
-  }
+    }
 
-  // TODO: Check Double precision and Float80 calculation
-  func testAtanAccuracy() {
+    // TODO: Check Double precision and Float80 calculation
+    func testAtanAccuracy() {
     testFunctionAccuracy(Complex<Float>.atan, Complex<Float>.atan_builtin, accuracy: 16)
     testFunctionAccuracy(Complex<Double>.atan, Complex<Double>.atan_builtin, accuracy: 16)
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
     //testFunctionAccuracy(Complex<Float80>.atan, Complex<Float80>.atan_builtin, accuracy: 16)
     #endif
-  }
+    }
 
-  func testAcoshAccuracy() {
+    func testAcoshAccuracy() {
     testFunctionAccuracy(Complex<Float>.acosh, Complex<Float>.acosh_builtin, accuracy: 16)
     testFunctionAccuracy(Complex<Double>.acosh, Complex<Double>.acosh_builtin, accuracy: 16)
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
     testFunctionAccuracy(Complex<Float80>.acosh, Complex<Float80>.acosh_builtin, accuracy: 16)
     #endif
-  }  
+    }
 
-  // TODO: Check
-  func testASinhAccuracy() {
+    // TODO: Check
+    func testASinhAccuracy() {
     testFunctionAccuracy(Complex<Float>.asinh, Complex<Float>.asinh_builtin, accuracy: 16)
     testFunctionAccuracy(Complex<Double>.asinh, Complex<Double>.asinh_builtin, accuracy: 16)
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
     testFunctionAccuracy(Complex<Float80>.asinh, Complex<Float80>.asinh_builtin, accuracy: 16)
     #endif
-  }
-    
-  func testAtanhAccuracy() {
+    }
+
+    func testAtanhAccuracy() {
     testFunctionAccuracy(Complex<Float>.atanh, Complex<Float>.atanh_builtin, accuracy: 16)
     testFunctionAccuracy(Complex<Double>.atanh, Complex<Double>.atanh_builtin, accuracy: 16)
     #if (arch(i386) || arch(x86_64)) && !os(Windows) && !os(Android)
     testFunctionAccuracy(Complex<Float80>.atanh, Complex<Float80>.atanh_builtin, accuracy: 16)
     #endif
-  }
+    }
 }
 #endif
